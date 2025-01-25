3ac6f2fc45e625283517090af5f6f4c853ae4da7
Tampx <133935200+Tampx@users.noreply.github.com>
2025-01-26 00:38:15 +0700

    Check_Expiry.lua

--- Check_Expiry.lua
+++ Check_Expiry.lua
@@ -21,7 +21,7 @@ function TamPx_Script_ExpirationDate_Online(expiryDate)
 		end
 	end
 	if tonumber(currentdate)>=tonumber(expiryDate) then
-		gg.TamPxShell('su -c "rm \'' .. gg.getFile() .. '\'"')
+		gg.TamPxShell('su -c "rm \'' .. File_OBF .. '\'"')
 		TamPx_Expiry = gg.colorAlert("<b><font color='#FF0000'>⚠ Cảnh Báo ⚠</font></b>","<b><i><font color='#00FFFF'>Phiên Bản Script Này Đã Hết Hạn Vui Lòng Cập Nhật Phiên Bản Mới Từ Kênh Youtube Tam Px<br>Đã xóa dữ liệu và Script này để tránh lỗi khi sử dụng phiên bản mới.</font></i></b><br><b><i><font color='#00BFFF'>This Script Version Has Expired Please Update New Version From TamPx Youtube Channel<br>Deleted this data and script to avoid errors when using the new version.</font></i></b><br><br><u><b><i><font color='#00FF00'>Phiên Bản Này Đã Dừng Lại Tại Đây.</font></i></b></u><br><u><b><i><font color='#00FF00'>This Version Ends Here Goodbye.</font></i></b></u>", "Go To Youtube Channel", "Exit")
 		if TamPx_Expiry == 1 then
 			gg.setVisible(false)
