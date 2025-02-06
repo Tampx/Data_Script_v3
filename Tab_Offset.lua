0a699800100190e4bd3346dc1c2615bd119ffd44
Tampx <133935200+Tampx@users.noreply.github.com>
2025-02-06 09:26:53 +0700

    Tab_Offset.lua

--- Tab_Offset.lua
+++ Tab_Offset.lua
@@ -1,23 +1,23 @@
 Tab_Offset = {
-	0x1298a3c,--Người Vàng: public Boolean get_IsGod() { }
-	0x4ac7ca4,--Đặt Lại TK Khách: public static Boolean get_ResetGuest() { }
-	0x12a29dc,--Nhảy Cao: public Single get_MaxJumpHeight() { }
-	0x54c615c,--Speed Game: public Single get_DeltaTime() { }
-	0x225d4c0,--Camera Phải: protected virtual Single HODILECKNDD(Player KAEHJEGFKBL, Vector3 HLAONGBLDJH, Vector3 ACLOJCLLLDK) { }
-	0x225dedc,--Camera Cao: protected virtual Single MFLHACPEBFK(Player KAEHJEGFKBL) { }
-	0x11efae8,--Camera Cao: public Boolean get_IsSwimSurf() { }
-	0x225c05c,--Camera Xa: public virtual Single get_OffsetForNormal() { }
-	0x232AA0,--Camera Fov
-	0x2754368,--Aim Fov 180
-	0x1CE0DC0,--Aim Scope
-	0x18373dc,--Aim Lock: public Single MLDACOFFLDN() { }
-	0x2C057B8,--No Delay
-	0x1878638,--No Recoil: public Boolean KJFONMEIBHA(ref MADMMIICBNN JEEIBOEGGPD, Single PJKMCGLHEEB, Vector3 AMINEOHOKBL, Boolean FKEAEIGPDML, Boolean LKBIBLODBGG) { }
-	0x1807710,--Nạp Đạn Nhanh: public virtual Single DNGONFLICGC() { }
-	0x157caf4,--Đớp máu nhanh: public Single get_FSModeUseMedikitFasterRate() { }
-	0x2615FC4,--Đổi súng bắn luôn: Search
-	0x523a9b0,--Người nổi tiếng: public Boolean IsCelebrity(UInt32 role) { }
-	0x129d1f4,--Bỏ qua rơi cao: public virtual Boolean IsIgnoreHighFalling() { }
-	0x3538ec8,--Không có dù: public DFKKGHCFGNM JDNPPHMDCFE() { }
-	0x179600c,--Hack Lag: protected override Void OnNetworkFixedUpdate() { }
+	0x1298a3c,
+	0x4ac7ca4,
+	0x12a29dc,
+	0x54c615c,
+	0x225d4c0,
+	0x225dedc,
+	0x11efae8,
+	0x225c05c,
+	0x0,
+	0x0,
+	0x0,
+	0x18373dc,
+	0x0,
+	0x1878638,
+	0x1807710,
+	0x157caf4,
+	0x2615FC4,
+	0x523a9b0,
+	0x129d1f4,
+	0x3538ec8,
+	0x179600c,
 }
